d25e6e6ca2c30cf8603a3ff455f7b46b17547873
Tiago Cardona <tiago.cardona@gmail.com>
2025-03-18 16:13:42 +0000

    teste commit

---
+++ testeGIT/testeGIT/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace testeGIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+        
+        
+        }
+
+    }
+}
